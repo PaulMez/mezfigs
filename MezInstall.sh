0a8c798c3417b5ac31f4683ed8b13e9f80ca7f58
Meza <meza.leet@gmail.com>
2022-01-09 15:36:21 +1100

    n/a

--- MezInstall.sh
+++ MezInstall.sh
@@ -1,42 +1,113 @@
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/PaulMez/mezfigs/master/MezInstall.sh?$RANDOM)" 
-RED='\033[38;5;004m'
-Bak='\033[48;5;015m'
-NC='\033[0m' # No Color
-printf "Testing ${RED}${BLU}colour${NC} with printf\n"
-echo "Testing ${BLU}colour${NC} with echo\n"
-echo "\nInstalling Mez Configs.............\n"
-# sudo apt-get update -yy
-# sudo apt-get upgrade -yy
-echo "\nInstalling requirements.............\n"
-echo "\n${RED}${BLU}1. wget:${NC}\n"
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/PaulMez/mezfigs/master/MezInstall.sh)" 
+Black='\033[0;30m'        # Black
+On_Cyan='\033[46m'        # Cyan BG
+Color_Off='\033[0m'       # Text Reset
+echo "${Black}${On_Cyan}\n[Installing Mez Configs]${Color_Off}\n"
+sudo apt-get update -yy
+sudo apt-get upgrade -yy
+echo "\nInstalling requirements\n"
+echo "\n1. wget:\n"
 sudo apt install wget -yy
 echo "\n2. zsh:\n"
-# sudo apt install zsh -yy
-# sudo apt install curl -yy
-# sudo apt install git -yy
-# sudo apt install unzip -yy
-# sudo apt install fontconfig -yy
+sudo apt install zsh -yy
+sudo apt install curl -yy
+sudo apt install git -yy
+sudo apt install unzip -yy
+sudo apt install fontconfig -yy
 
-# sudo apt install screenfetch -yy
+sudo apt install screenfetch -yy
 
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -yy
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -yy
 
 
 
-# sh -c "$(curl -fsSL https://git.io/zinit-install)" -yy
+sh -c "$(curl -fsSL https://git.io/zinit-install)" -yy
 
-# #add to /.zshrc
-# echo "zinit light zsh-users/zsh-autosuggestions" >> /.zshrc
-# echo "zinit light zsh-users/zsh-completions" >> /.zshrc
-# echo "zinit light zdharma-continuum/fast-syntax-highlighting" >> /.zshrc
+#add to /.zshrc
+echo "zinit light zsh-users/zsh-autosuggestions" >> /.zshrc
+echo "zinit light zsh-users/zsh-completions" >> /.zshrc
+echo "zinit light zdharma-continuum/fast-syntax-highlighting" >> /.zshrc
 
-# #Nerd Fonts
-# wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
-# unzip FiraCode.zip -d ~/.fonts
-# rm FiraCode.zip
-# fc-cache -fv
+#Nerd Fonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
+unzip FiraCode.zip -d ~/.fonts
+rm FiraCode.zip
+fc-cache -fv
 
-# #wget https://raw.githubusercontent.com/PaulMez/mezfigs/master/Ubuntu/.zshrc // Only if using my config?!?!?
-# #may not overwrite?
-# # ???chsh -s $(which zsh)????
+#wget https://raw.githubusercontent.com/PaulMez/mezfigs/master/Ubuntu/.zshrc // Only if using my config?!?!?
+#may not overwrite?
+# ???chsh -s $(which zsh)????
 
+
+# # Reset
+# Color_Off='\033[0m'       # Text Reset
+
+# # Regular Colors
+# Black='\033[0;30m'        # Black
+# Red='\033[0;31m'          # Red
+# Green='\033[0;32m'        # Green
+# Yellow='\033[0;33m'       # Yellow
+# Blue='\033[0;34m'         # Blue
+# Purple='\033[0;35m'       # Purple
+# Cyan='\033[0;36m'         # Cyan
+# White='\033[0;37m'        # White
+
+# # Bold
+# BBlack='\033[1;30m'       # Black
+# BRed='\033[1;31m'         # Red
+# BGreen='\033[1;32m'       # Green
+# BYellow='\033[1;33m'      # Yellow
+# BBlue='\033[1;34m'        # Blue
+# BPurple='\033[1;35m'      # Purple
+# BCyan='\033[1;36m'        # Cyan
+# BWhite='\033[1;37m'       # White
+
+# # Underline
+# UBlack='\033[4;30m'       # Black
+# URed='\033[4;31m'         # Red
+# UGreen='\033[4;32m'       # Green
+# UYellow='\033[4;33m'      # Yellow
+# UBlue='\033[4;34m'        # Blue
+# UPurple='\033[4;35m'      # Purple
+# UCyan='\033[4;36m'        # Cyan
+# UWhite='\033[4;37m'       # White
+
+# # Background
+# On_Black='\033[40m'       # Black
+# On_Red='\033[41m'         # Red
+# On_Green='\033[42m'       # Green
+# On_Yellow='\033[43m'      # Yellow
+# On_Blue='\033[44m'        # Blue
+# On_Purple='\033[45m'      # Purple
+# On_Cyan='\033[46m'        # Cyan
+# On_White='\033[47m'       # White
+
+# # High Intensity
+# IBlack='\033[0;90m'       # Black
+# IRed='\033[0;91m'         # Red
+# IGreen='\033[0;92m'       # Green
+# IYellow='\033[0;93m'      # Yellow
+# IBlue='\033[0;94m'        # Blue
+# IPurple='\033[0;95m'      # Purple
+# ICyan='\033[0;96m'        # Cyan
+# IWhite='\033[0;97m'       # White
+
+# # Bold High Intensity
+# BIBlack='\033[1;90m'      # Black
+# BIRed='\033[1;91m'        # Red
+# BIGreen='\033[1;92m'      # Green
+# BIYellow='\033[1;93m'     # Yellow
+# BIBlue='\033[1;94m'       # Blue
+# BIPurple='\033[1;95m'     # Purple
+# BICyan='\033[1;96m'       # Cyan
+# BIWhite='\033[1;97m'      # White
+
+# # High Intensity backgrounds
+# On_IBlack='\033[0;100m'   # Black
+# On_IRed='\033[0;101m'     # Red
+# On_IGreen='\033[0;102m'   # Green
+# On_IYellow='\033[0;103m'  # Yellow
+# On_IBlue='\033[0;104m'    # Blue
+# On_IPurple='\033[0;105m'  # Purple
+# On_ICyan='\033[0;106m'    # Cyan
+# On_IWhite='\033[0;107m'   # White
