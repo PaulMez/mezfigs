678b7e30f1da3b3c5701c49947ed9c0fa5837534
Meza <meza.leet@gmail.com>
2022-01-09 15:07:36 +1100

    n/a

--- MezInstall.sh
+++ MezInstall.sh
@@ -14,32 +14,32 @@ echo "Installing requirements.............\n"
 echo "1. wget:\n"
 sudo apt install wget -yy
 echo "2. zsh:\n"
-sudo apt install zsh -yy
-sudo apt install curl -yy
-sudo apt install git -yy
-sudo apt install unzip -yy
-sudo apt install fontconfig -yy
+# sudo apt install zsh -yy
+# sudo apt install curl -yy
+# sudo apt install git -yy
+# sudo apt install unzip -yy
+# sudo apt install fontconfig -yy
 
-sudo apt install screenfetch -yy
+# sudo apt install screenfetch -yy
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -yy
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -yy
 
 
 
-sh -c "$(curl -fsSL https://git.io/zinit-install)" -yy
+# sh -c "$(curl -fsSL https://git.io/zinit-install)" -yy
 
-#add to /.zshrc
-echo "zinit light zsh-users/zsh-autosuggestions" >> /.zshrc
-echo "zinit light zsh-users/zsh-completions" >> /.zshrc
-echo "zinit light zdharma-continuum/fast-syntax-highlighting" >> /.zshrc
+# #add to /.zshrc
+# echo "zinit light zsh-users/zsh-autosuggestions" >> /.zshrc
+# echo "zinit light zsh-users/zsh-completions" >> /.zshrc
+# echo "zinit light zdharma-continuum/fast-syntax-highlighting" >> /.zshrc
 
-#Nerd Fonts
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
-unzip FiraCode.zip -d ~/.fonts
-rm FiraCode.zip
-fc-cache -fv
+# #Nerd Fonts
+# wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
+# unzip FiraCode.zip -d ~/.fonts
+# rm FiraCode.zip
+# fc-cache -fv
 
-#wget https://raw.githubusercontent.com/PaulMez/mezfigs/master/Ubuntu/.zshrc // Only if using my config?!?!?
-#may not overwrite?
-# ???chsh -s $(which zsh)????
+# #wget https://raw.githubusercontent.com/PaulMez/mezfigs/master/Ubuntu/.zshrc // Only if using my config?!?!?
+# #may not overwrite?
+# # ???chsh -s $(which zsh)????
 
