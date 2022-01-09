ab69e077b6b97ca0d45d70ae17e1fb2c1c6d9cb4
Meza <meza.leet@gmail.com>
2022-01-09 15:07:18 +1100

    n/a

--- MezInstall.sh
+++ MezInstall.sh
@@ -1,13 +1,22 @@
 # sh -c "$(curl -fsSL https://raw.githubusercontent.com/PaulMez/mezfigs/master/MezInstall.sh)" 
-
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+printf "Testing ${RED}colour${NC} with printf\n"
+echo "Testing ${RED}colour${NC} with echo\n"
+echo "Installing Mez Configs.............\n"
+echo "Installing Mez Configs.............\n"
+echo "Installing Mez Configs.............\n"
+echo "Installing Mez Configs.............\n"
+echo "Updating and Upgrading.............\n"
 sudo apt-get update -yy
 sudo apt-get upgrade -yy
-
+echo "Installing requirements.............\n"
+echo "1. wget:\n"
 sudo apt install wget -yy
+echo "2. zsh:\n"
 sudo apt install zsh -yy
 sudo apt install curl -yy
 sudo apt install git -yy
-
 sudo apt install unzip -yy
 sudo apt install fontconfig -yy
 
@@ -17,7 +26,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 
 
 
-sh -c "$(curl -fsSL https://git.io/zinit-install)"
+sh -c "$(curl -fsSL https://git.io/zinit-install)" -yy
 
 #add to /.zshrc
 echo "zinit light zsh-users/zsh-autosuggestions" >> /.zshrc
