279368eac5f205299d943b92a080f72c24ab04b9
PaulMez <PaulMez@users.noreply.github.com>
2022-04-07 17:53:24 +1000

    Creation and Ideation

---
+++ MezFreshLinux.sh
@@ -0,0 +1,9 @@
+Dependicies
+Aliases
+Starship - Cross-Shell Prompt
+zplug - A next-generation plugin manager for zsh
+zsh-completions - Additional completion definitions for Zsh.
+zsh-you-should-use - ZSH plugin that reminds you to use existing aliases for commands you just typed.
+zsh-history-substring-search - ZSH port of Fish history search (up arrow)
+zsh-completions - Additional completion definitions for Zsh.
+zsh-autosuggestions - Fish-like autosuggestions for Zsh.
