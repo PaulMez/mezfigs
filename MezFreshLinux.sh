478dfad3aa169880adf7287f5e5b839bbce67c96
PaulMez <meza.leet@gmail.com>
2022-04-17 03:02:57 +0000

    n/a

--- MezFreshLinux.sh
+++ MezFreshLinux.sh
@@ -6,7 +6,8 @@
 
 #paste|| sudo apt-get install curl -yy; bash -c "$(curl -H 'Cache-Control: no-cache' -fsSL https://raw.githubusercontent.com/PaulMez/mezfigs/master/MezFreshLinux.sh)"
 
-
+#perfect prompt
+#https://raw.githubusercontent.com/b4b4r07/screenshots/master/zplug/demo.gif
 
 
 
@@ -38,7 +39,7 @@ MezPrint "Installing Individual Requirements"
 
 
 # Dependencies & Common Apps
-declare -a Reqs=("wget" "zsh" "git" "unzip" "fontconfig" "screenfetch" "cmatrix" "tmux")
+declare -a Reqs=("wget" "zsh" "git" "unzip" "fontconfig" "screenfetch" "cmatrix" "tmux" "awk")
 arraylength=${#Reqs[@]}
 
 for (( i=1; i<${arraylength}; i++ ));
@@ -55,11 +56,22 @@ rm FiraCode.zip
 fc-cache -fv
 
 # Aliases
+
 # Tmux configs/ shortcuts
-# Starship - Cross-Shell Prompt
+
+#Powerlevel10k (and config if prompted)
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+
+
 # zplug - A next-generation plugin manager for zsh
+$ curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+
 # zsh-completions - Additional completion definitions for Zsh.
 # zsh-you-should-use - ZSH plugin that reminds you to use existing aliases for commands you just typed.
 # zsh-history-substring-search - ZSH port of Fish history search (up arrow)
 # zsh-completions - Additional completion definitions for Zsh.
 # zsh-autosuggestions - Fish-like autosuggestions for Zsh.
+
+#Config P10k
+p10k configure
