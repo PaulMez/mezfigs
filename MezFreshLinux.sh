7fdef6312216617bbc0caf4f5622a27a6f44a59a
PaulMez <meza.leet@gmail.com>
2022-04-17 01:55:50 +0000

    n/a

--- MezFreshLinux.sh
+++ MezFreshLinux.sh
@@ -28,17 +28,9 @@ MezPrint () {
 echo -e "${MezBack}\n[$1]${reset}\n"
 }
 
-print_center(){
-    local x
-    local y
-    text="$*"
-    x=$(( ($(tput cols) - ${#text}) / 2))
-    echo -ne "\E[6n";read -sdR y; y=$(echo -ne "${y#*[}" | cut -d';' -f1)
-    echo -ne "\033[${y};${x}f$*"
-}
 
 clear
 MezPrint "-----------------[Installing Mez Configs]-----------------"
 
-print_center MezPrint "Installing Individual Requirements"
-print_center 'Hi'
+MezPrint "Installing Individual Requirements"
+echo "Some Text" | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols)
