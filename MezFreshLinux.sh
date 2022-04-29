77f6f4c0f5af0ff9b2c66339d29972651cc18269
PaulMez <meza.leet@gmail.com>
2022-04-29 12:09:57 +0000

    options

--- MezFreshLinux.sh
+++ MezFreshLinux.sh
@@ -71,16 +71,29 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 
 
-# zplug - A next-generation plugin manager for zsh
+# zplug - A next-generation plugin manager for zsh   or maybe use ZINIT?
 MezPrint "Installing Zplug & adding plugins"
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh #maybe dont initiate zsh yet?
+
+
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# My ~/.zshrc contains the following:
+
+# if [ -f ${HOME}/.zplug/init.zsh ]; then
+#     source ${HOME}/.zplug/init.zsh
+# fi
+
+/.zplug/init.zsh
 
 # zsh-completions - Additional completion definitions for Zsh.
 # zsh-you-should-use - ZSH plugin that reminds you to use existing aliases for commands you just typed.
 # zsh-history-substring-search - ZSH port of Fish history search (up arrow)
 # zsh-completions - Additional completion definitions for Zsh.
 # zsh-autosuggestions - Fish-like autosuggestions for Zsh.
+#Make zsh default
+chsh -s /usr/bin/zsh
+
 
 #Config P10k
 #zsh
-#p10k configure
+#p10k configure | n | Y | Y | 3 | 1 | 3 | 1 | 1 | 1 | 1 | 3 | 1 | Y | 1 | Y
