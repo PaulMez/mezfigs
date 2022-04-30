f0520c21f878867cf281d5184a670e2062a6a5fd
PaulMez <meza.leet@gmail.com>
2022-04-30 03:26:56 +0000

    zinit non finisdhed

--- MezFreshLinux.sh
+++ MezFreshLinux.sh
@@ -71,27 +71,17 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 
 
-# zplug - A next-generation plugin manager for zsh   or maybe use ZINIT?
-MezPrint "Installing Zplug & adding plugins"
-curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh #maybe dont initiate zsh yet?
+# # zplug - A next-generation plugin manager for zsh   or maybe use ZINIT?
+# MezPrint "Installing Zplug & adding plugins"
+# curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh #maybe dont initiate zsh yet?
 
-
-# #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-# # My ~/.zshrc contains the following:
-
-# # if [ -f ${HOME}/.zplug/init.zsh ]; then
-# #     source ${HOME}/.zplug/init.zsh
-# # fi
-
-# /.zplug/init.zsh
 
 # # zsh-completions - Additional completion definitions for Zsh.
 # # zsh-you-should-use - ZSH plugin that reminds you to use existing aliases for commands you just typed.
 # # zsh-history-substring-search - ZSH port of Fish history search (up arrow)
 # # zsh-completions - Additional completion definitions for Zsh.
 # # zsh-autosuggestions - Fish-like autosuggestions for Zsh. marlonrichert/zsh-autocomplete
-# #Make zsh default
-# chsh -s /usr/bin/zsh
+
 bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
 
 #add following to .zshrc
@@ -102,6 +92,9 @@ zinit load MichaelAquilina/zsh-you-should-use
 # #Config P10k
 # #zsh
 # #p10k configure | n | Y | Y | 3 | 1 | 3 | 1 | 1 | 1 | 1 | 3 | 1 | Y | 1 | Y
+
+# #Make zsh default
+# chsh -s /usr/bin/zsh
 
 
 # #Katoolin (optional)
