8a20bd404e7fba60bff00c358430460f6269d44d
Paul Mez <paulmez@Pauls-MacBook-Pro-2.local>
2024-03-05 22:10:32 +1100

    Added to Setup

--- MezSetupNew.sh
+++ MezSetupNew.sh
@@ -2,15 +2,28 @@
 #bash -c "$(curl -H 'Cache-Control: no-cache' -fsSL https://raw.githubusercontent.com/PaulMez/mezfigs/master/MezSetupNew.sh)" 
 #bash -c "$(curl -H 'Cache-Control: no-cache' -fsSL https://raw.githubusercontent.com/PaulMez/mezfigs/master/MezSetupNew.sh)"
 
+#functions
 MezBack='\e[46;30m'
+MezBackW='\e[37;30m'
+MezBackCy='\e[36;40m'
 reset='\e[0m'
 
-
 MezPrint () {
-echo -e "${MezBack}\n[$1]${reset}\n"
+echo -e "${MezBackCy}\n$1${reset}\n"
 }
 
-MezPrint "Installing Mez Configs"
+MezPrintCen () {
+echo -e "${MezBackCy}"
+echo $1 | sed  -e :a -e "s/^.\{1,$(tput cols)\}$/ & /;ta" | tr -d '\n' | head -c $(tput cols)
+echo -e "${reset}\n"
+}
+#intro
+clear
+MezPrintCen "[--------------------------------------------]"
+MezPrintCen "[Installing Mez Configs]"
+MezPrintCen "[--------------------------------------------]"
+MezPrint "Installing Individual Requirements" 
 
-
-MezPrint "Installing Individual Requirements"
+#update first
+MezPrint "Updating apt"
+sudo apt-get update
